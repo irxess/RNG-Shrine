a6766ed46cd8d578938aa9116313a34276475dae
irxess <irxess@gmail.com>
2014-02-05 19:03:49 +0100

    Changed d20 appearance. Added light

--- js/shrine.js
+++ js/shrine.js
@@ -1,19 +1,29 @@
 var camera, scene, renderer;
-var d20, material, mesh;
+var d20, dieMaterial, mesh;
+var i;
 
 init();
 animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    camera.position.z = 800; // how far zoomed out we are
 
     scene = new THREE.Scene();
 
     d20 = new THREE.IcosahedronGeometry( 120, 0 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
-    mesh = new THREE.Mesh( d20, material );
+    // wireframe: draw lines instead of coloring sides
+    dieMaterial = new THREE.MeshLambertMaterial( { color: 0xffcc00, wireframe: false } );
+    mesh = new THREE.Mesh( d20, dieMaterial );
     scene.add( mesh );
+
+    // Add lights
+    var leftLight = new THREE.PointLight( 0xffffff );
+    leftLight.position.set( -600, 0, 0 ); // x, y, z?
+    scene.add( leftLight );
+    var rightLight = new THREE.PointLight( 0xffffff );
+    rightLight.position.set( 600, 0, 0 ); // x, y, z?
+    scene.add( rightLight );
 
     renderer = new THREE.CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -22,9 +32,12 @@ function init() {
 }
 
 function animate() {
+    i = 0;
     requestAnimationFrame( animate );
-
-    mesh.rotation.y += 0.015;
+    
+    i += 0.01;
+    //mesh.rotation.y += 0.015;
+    mesh.rotation.y += Math.sin(i);
 
     renderer.render( scene, camera );
 }
