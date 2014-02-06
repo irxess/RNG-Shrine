ace7c1d213d77ea3b745a47b466684a4b7032fe6
irxess <irxess@gmail.com>
2014-02-06 18:31:12 +0100

    Changed hardcoded values to variable

--- js/shrine.js
+++ js/shrine.js
@@ -1,32 +1,45 @@
 var camera, scene, renderer;
-var d20, dieMaterial, mesh;
+var mesh;
+var poleR, poleR, podium, podiumTop, d20; // shapes
+var dieMaterial, podiumMaterial;
 var i;
+
+
+// sizes
+var cameraDistance = 800;
+var smallObjectsDetail = 10;
+var poleLength = 600;
+var leftPolePositionX = -500;
+var rightPolePositionX = -leftPolePositionX;
+var polePositionY = -200;
+var poleRadius = 10;
+var d20PositionY = 250;
 
 init();
 animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 800; // how far zoomed out we are
+    camera.position.z = cameraDistance; // how far zoomed out we are
 
     scene = new THREE.Scene();
 
     d20 = new THREE.IcosahedronGeometry( 120, 0 );
-    poleR = new THREE.CylinderGeometry(10, 10, 600, 8, 1, false);
-    poleL = new THREE.CylinderGeometry(10, 10, 600, 8, 1, false);
-    podium = new THREE.CylinderGeometry(100, 100, 500, 8, 1, false);
+    poleR = new THREE.CylinderGeometry(poleRadius, poleRadius, poleLength, smallObjectsDetail, 1, false);
+    poleL = new THREE.CylinderGeometry(poleRadius, poleRadius, poleLength, smallObjectsDetail, 1, false);
+    podium = new THREE.CylinderGeometry(100, 100, 500, 14, 1, false);
     podiumTop = new THREE.CylinderGeometry(150, 150, 50, 8, 1, false);
-    podiumMat = new THREE.MeshLambertMaterial( { color: 0xffcc00, wireframe: false } );
-    // wireframe: draw lines instead of coloring sides
+    var testSphere = new THREE.SphereGeometry( 100, smallObjectsDetail, smallObjectsDetail, Math.PI, Math.PI, Math.PI/2 );
+    podiumMaterial = new THREE.MeshLambertMaterial( { color: 0xffcc00, wireframe: false, emissive: 0xdddddd } ); // wireframe: draw lines instead of coloring sides
     dieMaterial = new THREE.MeshLambertMaterial( { wireframe: false, emissive: 0xaaaaaa } );
-    pRMesh = new THREE.Mesh( poleR , podiumMat );
-    pLMesh = new THREE.Mesh( poleL , podiumMat );
-    pMesh = new THREE.Mesh( podium , podiumMat );
-    ptMesh = new THREE.Mesh( podiumTop , podiumMat );
+    pRMesh = new THREE.Mesh( poleR , podiumMaterial );
+    pLMesh = new THREE.Mesh( poleL , podiumMaterial );
+    pMesh = new THREE.Mesh( podium , podiumMaterial );
+    ptMesh = new THREE.Mesh( podiumTop , podiumMaterial );
     d20mesh = new THREE.Mesh( d20, dieMaterial );
-    pRMesh.position.set(500,-200,0);
-    pLMesh.position.set(-500,-200,0);
-    d20mesh.position.set(0,250,0);
+    pRMesh.position.set( rightPolePositionX, polePositionY, 0);
+    pLMesh.position.set( leftPolePositionX, polePositionY, 0);
+    d20mesh.position.set( 0, d20PositionY, 0 );
     pMesh.position.set(0,-200,0);
     ptMesh.position.set(0,50,0);
     scene.add( d20mesh );
@@ -34,13 +47,17 @@ function init() {
     scene.add( pRMesh );
     scene.add( pLMesh );
     scene.add( ptMesh );
+    tmpMesh = new THREE.Mesh( testSphere, dieMaterial );
+    tmpMesh.material.side = THREE.DoubleSide;
+    tmpMesh.position.set(-300, 0, 0);
+    scene.add( tmpMesh );
 
     // Add lights
     var leftLight = new THREE.PointLight( 0x00ff00 );
-    leftLight.position.set( -600, 0, 0 ); // x, y, z?
+    leftLight.position.set( leftPolePositionX, 0, 0 );
     scene.add( leftLight );
     var rightLight = new THREE.PointLight( 0xff0000 );
-    rightLight.position.set( 600, 0, 0 ); // x, y, z?
+    rightLight.position.set( rightPolePositionX, 0, 0 );
     scene.add( rightLight );
 
     renderer = new THREE.CanvasRenderer();
